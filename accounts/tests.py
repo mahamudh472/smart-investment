6f40b14f4a18a99073eba6b2b986efd692710d8c
mahamudh472 <mahadymahamudh472@gmail.com>
2025-03-04 16:13:16 +0600

    adding tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestingTest(TestCase):
+    def setUp(self):
+        self.name="Mahmud"
+    def test_check_name(self):
+        self.assertEqual(self.name, "Mahmud")
